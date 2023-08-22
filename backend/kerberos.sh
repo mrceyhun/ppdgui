34f9774cf362269c1a0e828938fbb7ab47ef5935
mrceyhun <ceyhunuzngl@gmail.com>
2023-08-22 19:47:41 +0200

    Fix backend kerberos.sh, no need to define KRB5CCNAME env

--- backend/kerberos.sh
+++ backend/kerberos.sh
@@ -9,7 +9,6 @@ if [ "$1" = "-h" ] || [ "$1" = "-help" ] || [ "$1" = "--help" ] || [ "$1" = "hel
     exit 1
 fi
 
-export KRB5CCNAME=/tmp/krb5cc
 keytab=$1
 echo "using keytab=$keytab"
 principal=$(klist -k "$keytab" | tail -1 | awk '{print $2}')
